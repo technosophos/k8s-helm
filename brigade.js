bf858f2bd261b90eca3a810958f456f26a1d498e
Matt Butcher <matt.butcher@microsoft.com>
2018-08-12 15:20:01 -0600

    Add style, unit, and analysis tests.

--- brigade.js
+++ brigade.js
@@ -5,34 +5,106 @@ events.on("check_suite:rerequested", runSuite);
 events.on("check_run:rerequested", runSuite);
 
 function runSuite(e, p) {
-    runDCO(e, p);
-    //runTests(e, p);
-    //runDocs(e, p);
-    //runAnalytics(e, p);
+    runDCO(e, p).catch(e => {console.error(e.toString())});
+    runUnitTests(e, p).catch(e => {console.error(e.toString())});
+    runStyleTests(e, p).catch(e => {console.error(e.toString())});
+    runAnalytics(e, p).catch(e => {console.error(e.toString())});
 }
 
 // Run tests and fail if the tests do not pass.
-async function runTests(e, p) {
-    const command = "make bootstrap test";
+function runUnitTests(e, p) {
+    const command = "make bootstrap test-unit";
     var note = new Notification("tests", e, p);
     note.conclusion = "";
     note.title = "Run Tests"
     note.summary = "Running the test target for " + e.revision.commit;
-    await note.run();
 
-    var job = new GoJob("tests", e, p);
+    var job = new GoJob("run-tests", e, p);
+    job.tasks.push(command);
+    return notificationWrap(job, note)
+}
+
+async function runStyleTests(e, p) {
+    const command = "make bootstrap test-style";
+    var note = new Notification("style", e, p);
+    note.conclusion = "";
+    note.title = "Run Style Tests"
+    note.summary = "Running the style test target for " + e.revision.commit;
+    var job = new GoJob("run-style", e, p);
     job.tasks.push(command);
 
+    return notificationWrap(job, note);
+}
+
+// Run analytics, but don't fail unless something breaks internally.
+function runAnalytics(e, p) {
+    const commands = [
+        "make bootstrap",
+        "go get github.com/fzipp/gocyclo",
+        "go get honnef.co/go/tools/cmd/staticcheck",
+        "go get github.com/walle/lll/...",
+    `
+    set +e
+    for pkg in $(go list ./... | grep -v /vendor/); 
+      do
+        echo "==> $pkg";
+        gocyclo -over 10 $GOPATH/src/"$pkg";
+        lll --maxlength 120  $GOPATH/src/"$pkg";
+        staticcheck "$pkg";
+      done
+      exit 0`
+    ];
+    var note = new Notification("analytics", e, p);
+    note.conclusion = "";
+    note.title = "Run Style Tests"
+    note.summary = "Running the style test target for " + e.revision.commit;
+    var job = new GoJob("run-style", e, p);
+    Array.prototype.push.apply(job.tasks, commands);
+
+    return notificationWrap(job, note, "neutral");
+}
+
+// Not as cool as runDMC.
+function runDCO(e, p) {
+    const ghData = JSON.parse(e.payload);
+    var dco = new Notification("dco", e, p);
+    dco.title = "Developer Certificate of Origin (DCO)"
+
+    const re = new RegExp(/Signed off by:(.*)/, 'i');
+
+    // TODO: this should be a regexp.
+    const signedOff = re.exec(ghData.body.check_suite.head_commit.message);
+    if (signedOff == null){
+        dco.summary = "DCO check failed: Not signed off."
+        dco.text = "This commit is inelligible for merging until it is signed off. https://developercertificate.org/";
+        dco.conclusion = "failure";
+    } else {
+        dco.summary = "DCO check succeeded";
+        dco.text = `Commit signed by ${ signedOff[1] }`;
+        dco.conclusion = "success";
+    }
+    
+    console.log(dco.summary);
+    return dco.run();
+}
+
+async function notificationWrap(job, note, conclusion) {
+    if (conclusion == null) {
+        conclusion = "success"
+    }
+    await note.run();
     try {
         let res = await job.run()
+        const logs = await job.logs();
 
-        note.conclusion = "succeeded";
-        note.summary = "Testing run passed";
-        note.text = res.toString();
+        note.conclusion = conclusion;
+        note.summary = `Task "${ job.name }" passed`;
+        note.text = note.text = "```" + logs + "```\nSuccess: " + res.toString();
     } catch (e) {
+        const logs = await job.logs();
         note.conclusion = "failure";
-        note.summary = `Testing run failed for ${ e.buildID }`;
-        note.text = e.toString();
+        note.summary = `Task "${ job.name }" failed for ${ e.buildID }`;
+        note.text = "```" + logs + "```\nFailed with error: " + e.toString();
         try {
             return await note.run();
         } catch (e2) {
@@ -43,38 +115,6 @@ async function runTests(e, p) {
     }
 }
 
-// Generate docs and fail of the docs cannot be generated.
-function runDocs(e, p) {
-
-}
-
-// Run analytics, but don't fail unless something breaks internally.
-function runAnalytics(e, p) {
-
-}
-
-// Not as cool as runDMC.
-function runDCO(e, p) {
-    console.log(e.payload);
-    const ghData = JSON.parse(e.payload);
-    var dco = new Notification("dco", e, p);
-    dco.title = "Developer Certificate of Origin (DCO)"
-
-    // TODO: this should be a regexp.
-    const signedOff = ghData.body.check_suite.head_commit.message.indexOf("Signed off by:")
-    if (signedOff == -1){
-        dco.summary = "DCO check failed: Not signed off."
-        dco.text = "This commit is inelligible for merging until it is signed off. https://developercertificate.org/";
-        dco.conclusion = "failure";
-    } else {
-        dco.summary = "DCO check succeeded";
-        dco.conclusion = "succeeded";
-    }
-    
-    console.log(dco.summary);
-    return dco.run();
-}
-
 class Notification {
     constructor(name, e, p) {
         this.proj = p;
@@ -83,16 +123,23 @@ class Notification {
         this.externalID = e.buildID;
         this.detailsURL = `https://azure.github.com/kashti/builds/${ e.buildID }`;
 
-        // One of: "succeeded", "failure", "neutral", "canceled", or "timed_out".
+        // count allows us to send the notification multiple times, with a distinct pod name
+        // each time.
+        this.count = 0;
+
+        // One of: "success", "failure", "neutral", "cancelled", or "timed_out".
         this.conclusion = "neutral";
-        this.title = "runninc check";
+        this.title = "running check";
         this.text = "";
         this.summary = ""
     }
 
     // Send a new notification, and return a Promise<result>.
     run() {
-        const j = new Job(name, "technosophos/brigade-github-check-run:latest");
+        this.count++
+        console.log("create new job")
+        var j = new Job(`${ this.name }-${ this.count }`, "technosophos/brigade-github-check-run:latest");
+        console.log("set environment vars")
         j.env = {
             CHECK_CONCLUSION: this.conclusion,
             CHECK_NAME: this.name,
@@ -103,7 +150,8 @@ class Notification {
             CHECK_DETAILS_URL: this.detailsURL,
             CHECK_EXTERNAL_ID: this.externalID
         }
-        return j.run()
+        console.log("run job")
+        return j.run();
     }
 }
 
